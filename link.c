21be1055f0c59eedd57b020de0fd91d7779d0808
samsag6116 <samsag6116@gmail.com>
2017-09-14 11:15:05 -0400

    print list function added and check the errors

--- link.c
+++ link.c
@@ -3,7 +3,7 @@
   *  PROGRAMMER    : Sagar Patel
   *  FIRST VERSION : 13th SEP 2017
   *  DESCRIPTION   : I have written a simple c code to illustrate my concepts of data structure.
-  *			        It performs following basic operations 
+  *			         It performs following basic operations 
   *			 	          
   */
  
@@ -55,8 +55,32 @@
 	 tail->next = newnode;
 	 tail = newnode;
 	 return;
- }  
+ }
  
+ /*
+  *  FUNCTION      : print_list()
+  *
+  *  DESCRIPTION   : This function prints the all values in the list.
+  *					 First I take a current pointer to keep track of the node I am currently printing.
+  *					 After that I set the current pointer to next node and print again untill the next 
+  *					 is NULL.
+  *
+  *  PARAMETERS    : Nothing has been passed
+  *  
+  *  RETUEN        : Does not return anything
+  */
+ 
+ void print_list()
+ {
+	 node *current = head;
+	 
+	 while(current != NULL)
+	 {
+		 printf("%d\n", current->data);
+		 current = current->next;
+	 }
+ }
+  
  /*
  *  FUNCTION      : options()
  *
@@ -71,6 +95,8 @@ void options()
 	printf("\n YOUR CHOICES ");
 	printf("\n\n0 : To quit");
 	printf("\n1: To add a node");
+	printf("\n2: To print all the elements in list");
+
 }
 
 //********************************* Main Function  *************************************************************************//
@@ -91,6 +117,8 @@ void options()
 			 
 			 case 1: add_node();
 					 break;
+			 case 2: print_list();
+					 break;			
 				
 			 default: printf("There is no option available");
 		 }
