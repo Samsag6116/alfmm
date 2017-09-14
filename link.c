26f85a0c454b24d56380b774d6fc400b095cc0a0
samsag6116 <samsag6116@gmail.com>
2017-09-14 11:07:26 -0400

    First function add a node with test cases

--- link.c
+++ link.c
@@ -24,12 +24,78 @@
  
  node *head = NULL;
  node *tail = NULL;
+ 
+//********************************* Function Declarations *******************************************************************//
+/*
+ *  FUNCTION      : add_node()
+ *
+ *  DESCRIPTION   : This function add a node in link list.
+ *
+ *  PARAMETERS    : Nothing has been passed
+ *  
+ *  RETUEN        : Does not return anything
+ */
+ 
+ void add_node()
+ {
+	 if(head == NULL)
+	 {
+		 head = malloc(sizeof(node));
+		 printf("Enter value that you want to add :");
+		 scanf("%d",&(head->data));
+		 head->next = NULL;
+		 tail = head;
+		 return;
+	 }
+	 
+	 node *newnode = malloc(sizeof(node));
+	 printf("Enter value that you want to add :");
+	 scanf("%d",&(newnode->data));
+	 newnode->next = NULL;
+	 tail->next = newnode;
+	 tail = newnode;
+	 return;
+ }  
+ 
  /*
-  *  FUNCTION      : flashLed
-  *
-  *  DESCRIPTION   : This function makes led blink acoording to ledon and ledff value.
-  *
-  *  PARAMETERS    : int16_t ledOnTime = led on time period
-  *  
-  *  RETUEN        : NULL.
-  */
+ *  FUNCTION      : options()
+ *
+ *  DESCRIPTION   : Gives priority to user that which function they want to perform on link list.
+ *					
+ *  PARAMETERS    : Nothing has been passed
+ *  
+ *  RETUEN        : Does not return anything
+ */
+void options()
+{
+	printf("\n YOUR CHOICES ");
+	printf("\n\n0 : To quit");
+	printf("\n1: To add a node");
+}
+
+//********************************* Main Function  *************************************************************************//
+
+ int main()
+ {
+	 int select;
+	 
+	 options();
+	 printf("\nEnter your option :");
+	 scanf("%d",&select);
+	 
+	 while(select != 0)
+	 {
+		 switch(select)
+		 {
+			 case 0: printf("Quit");
+			 
+			 case 1: add_node();
+					 break;
+				
+			 default: printf("There is no option available");
+		 }
+ 		 options();
+	     printf("\nEnter your option :");
+	     scanf("%d",&select);
+	 }
+ }
